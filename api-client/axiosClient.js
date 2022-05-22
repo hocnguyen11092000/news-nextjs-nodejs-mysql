e09ec09355632bb2ed296d9b0b49b7343529f704
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-05-22 18:47:49 +0700

    change

--- api-client/axiosClient.js
+++ api-client/axiosClient.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // }
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://orchid.tk/api",
 });
 
 // // Add a request interceptor
